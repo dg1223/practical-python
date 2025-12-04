64b2ee8c6df89db91c8a49a809d99d4322287b06
dg1223 <alavi1223@hotmail.com>
2025-12-04 09:18:51 -0500

    bounce exercise

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce = 1
+
+while height > 0 and bounce <= 10:
+    height = round(height * 0.6, 4)
+    bounce += 1
+
+    print(f"{bounce-1} {height}")
