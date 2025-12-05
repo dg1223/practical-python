7912af885b0fee2315797167f8f5ed92171828d9
dg1223 <alavi1223@hotmail.com>
2025-12-05 00:30:48 -0500

    pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('Data/portfolio.csv', 'rt') as f:
+    total = 0
+    counter = 0
+
+    for line in f:
+        if counter == 0:
+            counter += 1
+            continue
+
+        row = line.split(',')
+        total += float(row[2]) * int(row[1])
+
+    print(f"Total cost: {total:.2f}")
