8e3d070499816fd2dce68f802c04a9f667af17da
dg1223 <alavi1223@hotmail.com>
2025-12-05 01:04:52 -0500

    report dict

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,42 @@
 # report.py
 #
 # Exercise 2.4
+
+def read_portfolio(filename):
+    with open(filename, 'rt') as f:
+        portfolio = []
+        counter = 0
+
+        for line in f:
+            if counter == 0:
+                counter += 1 # skip header
+                continue
+
+            row = line.split(',')
+            portfolio.append((row[0], int(row[1]), float(row[2])))
+
+    return portfolio
+
+def read_portfolio_dict(filename):
+    with open(filename, 'rt') as f:
+        portfolio = []
+        share_dict = {}
+        counter = 0
+
+        for line in f:
+            if counter == 0:
+                counter += 1 # skip header
+                continue
+
+            row = line.split(',')
+            
+            share_dict['name'] = row[0]
+            share_dict['shares'] = int(row[1])
+            share_dict['price'] = float(row[2])
+            portfolio.append(share_dict)
+
+    return portfolio
+
+if __name__ == "__main__":
+    # print(read_portfolio('Data/portfolio.csv'))
+    print(read_portfolio_dict('Data/portfolio.csv'))
